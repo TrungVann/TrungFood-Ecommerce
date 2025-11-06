4d51f790ba1caa5e4d6b4701fb8e4a65af226f9a
Nguyễn Văn Trung <trungheo100@gmail.com>
2025-11-07 03:51:11 +0700

    setup axios instance

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import {
+  getUser,
   loginUser,
   refreshToken,
   resetUserPassword,
@@ -8,6 +9,7 @@ import {
   verifyUser,
   verifyUserForgotPassword,
 } from "../controller/auth.controller";
+import isAuthenticated from "@packages/middleware/isAuthenticated";
 
 const router: Router = express.Router();
 
@@ -15,6 +17,7 @@ router.post("/user-registration", userRegistration);
 router.post("/verify-user", verifyUser);
 router.post("/login-user", loginUser);
 router.post("/refresh-token-user", refreshToken)
+router.get("/logged-in-user", isAuthenticated, getUser)
 router.post("/forgot-password-user", userForgotPassword);
 router.post("/reset-password-user", resetUserPassword);
 router.post("/verify-forgot-password-user", verifyUserForgotPassword);
