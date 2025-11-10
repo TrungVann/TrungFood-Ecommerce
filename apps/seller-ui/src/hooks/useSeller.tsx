8754f1756602c7a6a0958f7d09182770ecce36d5
Nguyễn Văn Trung <trungheo100@gmail.com>
2025-11-11 00:09:35 +0700

    setup useSeller hook

--- apps/seller-ui/src/hooks/useSeller.tsx
+++ apps/seller-ui/src/hooks/useSeller.tsx
@@ -1,27 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
-import axiosInstance from "../utils/axiosInstance";
+import axiosInstance from "apps/seller-ui/src/utils/axiosInstance";
 
-//Fetch user data from API
-const fetchUser = async () => {
-  const response = await axiosInstance.get("/api/logged-in-user");
+//Fetch seller data from API
+const fetchSeller = async () => {
+  const response = await axiosInstance.get("/api/logged-in-seller");
 
-  return response.data.user;
+  return response.data.seller;
 };
 
-const useUser = () => {
+const useSeller = () => {
   const {
-    data: user,
+    data: seller,
     isLoading,
     isError,
     refetch,
   } = useQuery({
-    queryKey: ["user"],
-    queryFn: fetchUser,
+    queryKey: ["seller"],
+    queryFn: fetchSeller,
     staleTime: 1000 * 60 * 5,
     retry: 1,
   });
 
-  return { user, isLoading, isError, refetch };
+  return { seller, isLoading, isError, refetch };
 };
 
-export default useUser;
+export default useSeller;
