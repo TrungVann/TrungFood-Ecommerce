29d2c5bcf3d265592ea595871f7786fbc1c35e72
Nguyễn Văn Trung <trungheo100@gmail.com>
2025-11-16 17:21:53 +0700

    setup color picker for product

--- apps/seller-ui/src/app/(routes)/dashboard/create-product/page.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/create-product/page.tsx
@@ -2,6 +2,7 @@
 
 import ImagePlaceHolder from "apps/seller-ui/src/shared/components/Image-placeholder";
 import { ChevronRight } from "lucide-react";
+import ColorSelector from "packages/components/color-selector";
 import Input from "packages/components/input";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -112,6 +113,110 @@ const Page = () => {
                 placeholder="Enter product title"
                 {...register("title", { required: "Title is required" })}
               />
+              {errors.title && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.title.message as string}
+                </p>
+              )}
+
+              <div className="mt-2">
+                <Input
+                  type="textarea"
+                  rows={7}
+                  cols={10}
+                  label="Short Description * (Max 150 words)"
+                  placeholder="Enter product description"
+                  {...register("description", {
+                    required: "Description is required",
+                    validate: (value) => {
+                      const wordCount = value.trim().split(/\s+/).length;
+                      return (
+                        wordCount <= 150 ||
+                        `Description cannot exceed 150 words (Current: ${wordCount})`
+                      );
+                    },
+                  })}
+                />
+                {errors.description && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.description.message as string}
+                  </p>
+                )}
+              </div>
+
+              <div className="mt-2">
+                <Input
+                  label="Tags *"
+                  placeholder="fruit, food,..."
+                  {...register("tags", {
+                    required: "Separate related products tags with a coma,",
+                  })}
+                />
+                {errors.tags && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.tags.message as string}
+                  </p>
+                )}
+              </div>
+
+              <div className="mt-2">
+                <Input
+                  label="Warranty *"
+                  placeholder="1 Year / No Warranty"
+                  {...register("warranty", {
+                    required: "Warranty is required",
+                  })}
+                />
+                {errors.tags && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.tags.message as string}
+                  </p>
+                )}
+              </div>
+
+              <div className="mt-2">
+                <Input
+                  label="Slug *"
+                  placeholder="product_slug"
+                  {...register("slug", {
+                    required: "Slug is required!",
+                    pattern: {
+                      value: /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
+                      message: "Invalid slug format!",
+                    },
+                    minLength: {
+                      value: 3,
+                      message: "Slug must be at least 3 characters long.",
+                    },
+                    maxLength: {
+                      value: 50,
+                      message: "Slug cannot be longer than 50 characters.",
+                    },
+                  })}
+                />
+                {errors.slug && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.slug.message as string}
+                  </p>
+                )}
+              </div>
+
+              <div className="mt-2">
+                <Input
+                  label="Brand"
+                  placeholder="Brand"
+                  {...register("brand")}
+                />
+                {errors.brand && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.brand.message as string}
+                  </p>
+                )}
+              </div>
+
+              <div className="mt-2">
+                <ColorSelector control={control} errors={errors}/>
+              </div>
             </div>
           </div>
         </div>
