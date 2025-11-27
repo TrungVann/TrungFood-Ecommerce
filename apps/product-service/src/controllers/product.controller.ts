220def0cfd2636f1a76a1ca59c4bd63f2e858711
Nguyễn Văn Trung <trungheo100@gmail.com>
2025-11-27 21:03:33 +0700

    finish function create discount codes

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -1,3 +1,4 @@
+import { ValidationError } from "@packages/error-handler";
 import prisma from "@packages/libs/prisma";
 import { NextFunction, Request, Response } from "express";
 
@@ -20,5 +21,42 @@ export const getCategories = async (
     });
   } catch (error) {
     return next(error);
+  }
+};
+
+// create discount codes
+export const createDiscountCode = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { public_name, discountType, discountValue, discountCode } = req.body;
+
+    const isDiscountCodeExist = await prisma.discount_codes.findUnique({
+      where: { discountCode },
+    });
+
+    if (!isDiscountCodeExist) {
+      return next(
+        new ValidationError(
+          "Discount code already please use a different code!"
+        )
+      );
+    }
+
+    const discount_code = await prisma.discount_codes.create({
+      data: {
+        public_name,
+        discountType,
+        discountValue: parseFloat(discountValue),
+        discountCode,
+        sellerId: req.seller.id,
+      },
+    });
+
+    res.status(201).json({ success: true, discount_code });
+  } catch (error) {
+    next(error);
   }
 };
