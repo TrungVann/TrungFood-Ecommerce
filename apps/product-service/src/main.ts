3401943a903991875a419ee21ee92cf46308f9cb
Nguyễn Văn Trung <trungheo100@gmail.com>
2025-12-10 09:26:21 +0700

    finish setup cron for project

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -1,10 +1,11 @@
-// import swaggerUi from "swagger-ui-express";
-// const swaggerDocument = require("./swagger-output.json");
+import swaggerUi from "swagger-ui-express";
+const swaggerDocument = require("./swagger-output.json");
 import express from "express";
 import cors from "cors";
 import { errorMiddleware } from "@packages/error-handler/error-middleware";
 import cookieParser from "cookie-parser";
 import router from "./routes/product.routes";
+import "./jobs/product-cron.job";
 
 const app = express();
 
@@ -24,10 +25,10 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello Product API" });
 });
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.get("/docs-json", (req, res) => {
-//   res.json(swaggerDocument);
-// });
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/docs-json", (req, res) => {
+  res.json(swaggerDocument);
+});
 
 //Router
 app.use("/api", router);
